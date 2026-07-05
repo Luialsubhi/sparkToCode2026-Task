75e306a03183edb183e629e09c2bc17e85739705
Luialsubhi <luialsubhi77@gmail.com>
2026-07-05 17:48:38 +0400

    power and root example

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,16 +4,25 @@
     {
         static void Main(string[] args)
         {
-            //Absolute Difference
-            Console.WriteLine("Enter the first number:");
-            float num1 = float.Parse(Console.ReadLine());
+            ////Absolute Difference
+            //Console.WriteLine("Enter the first number:");
+            //float num1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number:");
-            float num2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //float num2 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine($"The absolute difference between {num1} and {num2} is: {Math.Abs(num2 - num1)}");
+            //Console.WriteLine($"The absolute difference between {num1} and {num2} is: {Math.Abs(num2 - num1)}");
 
             ///////////////////////////////////////////////////////////////////
+            
+            //power and root example
+            Console.WriteLine("Enter the number:");
+            float number = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("the power of the number is: " + Math.Pow(number, 2));
+            Console.WriteLine("the root of the number is: " + Math.Sqrt(number));
+
+            /////////////////////////////////////////////////////////////////////
         }
     }
 }

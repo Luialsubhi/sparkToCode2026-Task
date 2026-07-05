eedf4f93a941ef91afaf290f19610f210c4639ad
Luialsubhi <luialsubhi77@gmail.com>
2026-07-05 18:00:41 +0400

    Name Formatter

--- task3/Program.cs
+++ task3/Program.cs
@@ -16,13 +16,25 @@
             ///////////////////////////////////////////////////////////////////
             
             //power and root example
-            Console.WriteLine("Enter the number:");
-            float number = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the number:");
+            //float number = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("the power of the number is: " + Math.Pow(number, 2));
-            Console.WriteLine("the root of the number is: " + Math.Sqrt(number));
+            //Console.WriteLine("the power of the number is: " + Math.Pow(number, 2));
+            //Console.WriteLine("the root of the number is: " + Math.Sqrt(number));
 
             /////////////////////////////////////////////////////////////////////
+            
+            //Name Formatter
+            Console.WriteLine("Enter your full name:");
+            string fullName = Console.ReadLine();
+
+            string input = fullName.ToUpper();
+            string input2 = fullName.ToLower();
+            int length = fullName.Length;
+
+            Console.WriteLine("Your name in uppercase: " + input);
+            Console.WriteLine("Your name in lowercase: " + input2);
+            Console.WriteLine("The length of your name is: " + length);
         }
     }
 }

e1a2f2f27e34ef7795453e273c948e50bdf2a00e
Luialsubhi <luialsubhi77@gmail.com>
2026-07-07 02:11:25 +0400

    spuare number function

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,22 +2,36 @@
 {
     internal class Program
     {
-        //personailzed Welcome Function
+        
         public static void PrintWelcome(string name)
+
         {
             Console.WriteLine("welcome " + name);
             
         }
+        public static int aSquare(int num )
+        {
+            return num*num;
+
+        }
         static void Main(string[] args)
         {
-            //personailzed Welcome Function
-            Console.WriteLine("enrer your name :");
-            string userName = Console.ReadLine();
-            PrintWelcome(userName);
+            ////personailzed Welcome Function
+            //Console.WriteLine("enrer your name :");
+            //string userName = Console.ReadLine();
+            //PrintWelcome(userName);
 
             ///////////////////////////////////////////////////////////////
-            
 
+            //spuare number function
+
+            Console.WriteLine("enter the number");
+            int input=Convert.ToInt32(Console.ReadLine());
+            int result =aSquare(input);
+            Console.WriteLine("the number is " + result);
+            
+            
+            /////////////////////
 
             
         }

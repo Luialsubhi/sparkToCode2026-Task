5760b6aaf75e9daa5ee680a06eae27f76de2a298
Luialsubhi <luialsubhi77@gmail.com>
2026-07-08 18:53:12 +0400

    Fixed Grade Array

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Fixed Grade Array
+            int[] grades = new int[5];
+            for (int i = 0; i < grades.Length; i++)
+            { 
+                Console.WriteLine("enter the grade :"+(i+1));
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("nstudent grade :");
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            
         }
     }
 }

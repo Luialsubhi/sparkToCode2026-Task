64996c4c6fabd78ff27414159f0d2f60e95dc3ee
Luialsubhi <luialsubhi77@gmail.com>
2026-07-08 19:53:13 +0400

    Browsing History Stack

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -19,23 +19,38 @@
             //}
             /////////////////////////////////////////////////////////////
             
-            //Dynamic To-Do List
-            List<string> Items = new List<string>();
-            for (int j = 0; j < 5; j++)
-            {
+            ////Dynamic To-Do List
+            //List<string> Items = new List<string>();
+            //for (int j = 0; j < 5; j++)
+            //{
 
-                Console.WriteLine("enter the item " +(j+1));
-                string item = Console.ReadLine();
-                Items.Add(item);
+            //    Console.WriteLine("enter the item " +(j+1));
+            //    string item = Console.ReadLine();
+            //    Items.Add(item);
 
-            }
-            Console.WriteLine("To-Do list:");
-            int number = 1;
-            foreach (string item in Items)
+            //}
+            //Console.WriteLine("To-Do list:");
+            //int number = 1;
+            //foreach (string item in Items)
+            //{
+            //    Console.WriteLine(number + " - " + item);
+            //    number++;
+            //}
+            /////////////////////////////////////////////////////////////////
+            
+            //Browsing History Stack
+            Stack<string> URLs = new Stack<string>();
+            for (int k = 0; k < 3; k++)
             {
-                Console.WriteLine(number + " - " + item);
-                number++;
+                Console.WriteLine("enter the URL "+(k+1));
+                string url =Console.ReadLine();
+                URLs.Push(url);
             }
+            URLs.Pop();
+
+            Console.WriteLine("this urls");
+            Console.WriteLine(URLs.Peek());
+
         }
     }
 }

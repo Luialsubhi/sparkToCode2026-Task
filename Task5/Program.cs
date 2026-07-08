96e73894c3560433bea0d8554d255bef314d7393
Luialsubhi <luialsubhi77@gmail.com>
2026-07-08 19:19:26 +0400

    Dynamic To-Do List

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,21 +4,38 @@
     {
         static void Main(string[] args)
         {
-            //Fixed Grade Array
-            int[] grades = new int[5];
-            for (int i = 0; i < grades.Length; i++)
-            { 
-                Console.WriteLine("enter the grade :"+(i+1));
-                grades[i] = int.Parse(Console.ReadLine());
-            }
-            Console.WriteLine("nstudent grade :");
+            ////Fixed Grade Array
+            //int[] grades = new int[5];
+            //for (int i = 0; i < grades.Length; i++)
+            //{ 
+            //    Console.WriteLine("enter the grade :"+(i+1));
+            //    grades[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("nstudent grade :");
 
-            foreach (int grade in grades)
-            {
-                Console.WriteLine(grade);
-            }
-
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            /////////////////////////////////////////////////////////////
             
+            //Dynamic To-Do List
+            List<string> Items = new List<string>();
+            for (int j = 0; j < 5; j++)
+            {
+
+                Console.WriteLine("enter the item " +(j+1));
+                string item = Console.ReadLine();
+                Items.Add(item);
+
+            }
+            Console.WriteLine("To-Do list:");
+            int number = 1;
+            foreach (string item in Items)
+            {
+                Console.WriteLine(number + " - " + item);
+                number++;
+            }
         }
     }
 }

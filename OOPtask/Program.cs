ff21c5fd855cbddc1e44fe8492c0097b3fedcccf
Luialsubhi <luialsubhi77@gmail.com>
2026-07-12 21:50:15 +0400

    class BankAccount

--- OOPtask/Program.cs
+++ OOPtask/Program.cs
@@ -1,10 +1,57 @@
 ﻿namespace OOPtask
 {
-    internal class Program
+     class BankAccount
+    {
+        public int AccountNumber; 
+        public string HolderName;
+        public double Balance;
+
+        public void Deposit (double amount) 
+        { 
+            Balance += amount;
+            SendEmail();
+        }
+        public void Withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                SendEmail();
+            }
+            else
+            {
+                Console.WriteLine("insufficent balance");
+            }
+        }
+        public double ChaeckBalance()
+        {
+            printInformation();
+            return Balance;
+
+        }
+        public void printInformation()
+        {
+            Console.WriteLine("holder name" + HolderName);
+            Console.WriteLine("balance: "+Balance);
+        }
+        public void SendEmail()
+        {
+            Console.WriteLine("email intinfication sent ");
+        }
+    }
+    public class Student
+    {
+
+    }
+    public class product
+    {
+
+    }
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }

28b014916ee824bb5b41eee00f9378a200a38b25
Luialsubhi <luialsubhi77@gmail.com>
2026-07-13 13:32:22 +0400

    case 7

--- OOPtask/Program.cs
+++ OOPtask/Program.cs
@@ -137,149 +137,167 @@
             Console.WriteLine("object created successfully");
             Console.ReadLine();
 
-            /////////////////////////////////////////////////////////////
-            //case 1
-            Console.WriteLine("choose");
-            Console.WriteLine("1.Karim");
-            Console.WriteLine("2. Ali");
+            ///////////////////////////////////////////////////////////////
+            ////case 1
+            //Console.WriteLine("choose");
+            //Console.WriteLine("1.Karim");
+            //Console.WriteLine("2. Ali");
 
-            int choice = Convert.ToInt32(Console.ReadLine());
-            if (choice == 1)
+            //int choice = Convert.ToInt32(Console.ReadLine());
+            //if (choice == 1)
+            //{
+            //    account1.ChaeckBalance();
+            //}
+            //else if (choice == 2) 
+            //{ 
+            //    account2.ChaeckBalance();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid choise");
+            //}
+            //Console.ReadLine();
+
+            ///////////////////////////////////////////////////////////////
+            ////case 2
+            //Console.WriteLine("choose");
+            //Console.WriteLine("1.Ali");
+            //Console.WriteLine("2. Ahmed");
+
+            //int choice2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter the new adderse");
+            //string newAddress = Console.ReadLine();
+
+            //if (choice2 == 1)
+            //{
+            //    student1.Address = newAddress;
+            //    Console.WriteLine("new adders   " + student1.Address);
+
+            //}
+            //else if (choice2 == 2)
+            //{
+            //    student2.Address = newAddress;
+            //    Console.WriteLine("new addres" + student2.Address);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid");
+            //}
+
+            ///////////////////////////////////////////////////////////
+            ////case 3
+            //Console.WriteLine("choose");
+            //Console.WriteLine("1.Karim");
+            //Console.WriteLine("2. Ali");
+
+            //int choice3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter the amount ");
+            //double amount = Convert.ToDouble(Console.ReadLine());
+
+            //if (choice == 1)
+            //{
+            //    account1.Deposit(amount);
+            //    Console.WriteLine("holder" + account1.HolderName);
+            //    Console.WriteLine("balance" + account1.Balance);
+
+            //}
+            //else if (choice == 2)
+            //{
+            //    account1.Deposit(amount);
+            //    Console.WriteLine("holder" + account2.HolderName);
+            //    Console.WriteLine("balance" + account2.Balance);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid choise");
+            //}
+            ///////////////////////////////////////////////////////////////
+            ////case 4
+            //Console.WriteLine("choose");
+            //Console.WriteLine("1.Karim");
+            //Console.WriteLine("2. Ali");
+
+            //int choice4 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter the withdraw ");
+            //double amount4 = Convert.ToDouble(Console.ReadLine());
+
+            //if (choice == 1)
+            //{
+            //    account1.Withdraw(amount4);
+
+            //    Console.WriteLine("balance" + account1.Balance);
+
+            //}
+            //else if (choice == 2)
+            //{
+            //    account1.Withdraw(amount4);
+
+            //    Console.WriteLine("balance" + account2.Balance);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid choise");
+            //}
+            //////////////////////////////////////////////////////////////
+            ////case 5
+            //Console.WriteLine("choose");
+            //Console.WriteLine("mose");
+            //Console.WriteLine("kebord");
+
+            //int choice5 = Convert.ToInt32(Console.ReadLine());
+            //if (choice5 == 1)
+            //{
+            //    Console.WriteLine("inventory value" + product1.GetInventoryValue());
+            //}
+            //else if (choice5 == 2) 
+            //{
+            //    Console.WriteLine("inventory value" + product2.GetInventoryValue());
+            //}
+            //else { Console.WriteLine("invald"); }
+            /////////////////////////////////////////////////////////////////////////
+            ////case6
+            //Console.WriteLine("choose");
+            //Console.WriteLine("1.Ali");
+            //Console.WriteLine("2.Ahmed");
+
+            //int choice6 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("enter email");
+            //string email = Console.ReadLine();
+
+            //if (choice6 == 1)
+            //{
+            //    student1.Register(email);
+            //    Console.WriteLine("student registered successfully");
+            //}
+            //else if (choice6 == 2)
+            //{
+
+            //    student2.Register(email);
+            //    Console.WriteLine("student registered successfully");
+            //} 
+            //else 
+            //{
+            //    Console.WriteLine("invalid");
+            //}
+            /////////////////////////////////////////////////////////////////
+            //case 7
+
+            if (account1.Balance > account2.Balance)
             {
-                account1.ChaeckBalance();
+                Console.WriteLine(account1.HolderName + "  more meny");
             }
-            else if (choice == 2) 
-            { 
-                account2.ChaeckBalance();
+            else if(account1.Balance < account2.Balance)
+            {
+                Console.WriteLine(account2.HolderName + "  more many");
             }
             else
             {
-                Console.WriteLine("invalid choise");
+                Console.WriteLine("equal");
             }
-            Console.ReadLine();
-
-            /////////////////////////////////////////////////////////////
-            //case 2
-            Console.WriteLine("choose");
-            Console.WriteLine("1.Ali");
-            Console.WriteLine("2. Ahmed");
-
-            int choice2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter the new adderse");
-            string newAddress = Console.ReadLine();
-
-            if (choice2 == 1)
-            {
-                student1.Address = newAddress;
-                Console.WriteLine("new adders   " + student1.Address);
-
-            }
-            else if (choice2 == 2)
-            {
-                student2.Address = newAddress;
-                Console.WriteLine("new addres" + student2.Address);
-            }
-            else
-            {
-                Console.WriteLine("invalid");
-            }
-
-            /////////////////////////////////////////////////////////
-            //case 3
-            Console.WriteLine("choose");
-            Console.WriteLine("1.Karim");
-            Console.WriteLine("2. Ali");
-
-            int choice3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter the amount ");
-            double amount = Convert.ToDouble(Console.ReadLine());
-
-            if (choice == 1)
-            {
-                account1.Deposit(amount);
-                Console.WriteLine("holder" + account1.HolderName);
-                Console.WriteLine("balance" + account1.Balance);
-
-            }
-            else if (choice == 2)
-            {
-                account1.Deposit(amount);
-                Console.WriteLine("holder" + account2.HolderName);
-                Console.WriteLine("balance" + account2.Balance);
-            }
-            else
-            {
-                Console.WriteLine("invalid choise");
-            }
-            /////////////////////////////////////////////////////////////
-            //case 4
-            Console.WriteLine("choose");
-            Console.WriteLine("1.Karim");
-            Console.WriteLine("2. Ali");
-
-            int choice4 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter the withdraw ");
-            double amount4 = Convert.ToDouble(Console.ReadLine());
-
-            if (choice == 1)
-            {
-                account1.Withdraw(amount4);
+            ///////////////////////////////////////////////////////////////////
                 
-                Console.WriteLine("balance" + account1.Balance);
-
-            }
-            else if (choice == 2)
-            {
-                account1.Withdraw(amount4);
-                
-                Console.WriteLine("balance" + account2.Balance);
-            }
-            else
-            {
-                Console.WriteLine("invalid choise");
-            }
-            ////////////////////////////////////////////////////////////
-            //case 5
-            Console.WriteLine("choose");
-            Console.WriteLine("mose");
-            Console.WriteLine("kebord");
-
-            int choice5 = Convert.ToInt32(Console.ReadLine());
-            if (choice5 == 1)
-            {
-                Console.WriteLine("inventory value" + product1.GetInventoryValue());
-            }
-            else if (choice5 == 2) 
-            {
-                Console.WriteLine("inventory value" + product2.GetInventoryValue());
-            }
-            else { Console.WriteLine("invald"); }
-            ///////////////////////////////////////////////////////////////////////
-            //case6
-            Console.WriteLine("choose");
-            Console.WriteLine("1.Ali");
-            Console.WriteLine("2.Ahmed");
-
-            int choice6 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("enter email");
-            string email = Console.ReadLine();
-
-            if (choice6 == 1)
-            {
-                student1.Register(email);
-                Console.WriteLine("student registered successfully");
-            }
-            else if (choice6 == 2)
-            {
-
-                student2.Register(email);
-                Console.WriteLine("student registered successfully");
-            } 
-            else 
-            {
-                Console.WriteLine("invalid");
-            }
-            }
+            
+        }
     }
 }

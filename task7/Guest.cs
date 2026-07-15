58ae76716ab0984c3a5f962e2ea5011bd6d2d69c
Luialsubhi <luialsubhi77@gmail.com>
2026-07-15 10:42:58 +0400

    class room and class guest

---
+++ task7/Guest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    internal class Guest
+    {
+        public string guestId;
+        public string guestName;
+        public string roomNumber;
+        public string checkInDats;
+        public int totalNights;
+
+        public void displayGuest()
+        {
+            Console.WriteLine("Guest Id  :" + guestId);
+            Console.WriteLine("Guest name  : "+guestName);
+            Console.WriteLine("room number  " + roomNumber);
+            Console.WriteLine("total"+totalNights);
+
+        }
+        public double calculateTotalCost(double pricePerNight)
+        {
+            return pricePerNight * totalNights;
+        }
+    }
+}
--- task7/Room.cs
+++ task7/Room.cs
@@ -8,5 +8,18 @@ namespace task7
 {
     internal class Room
     {
+        public int roomNumber;
+        public string roomType;
+        public double pricePerNight;
+        public bool isAvaiable;
+
+        public void displayRoom()
+        {
+            Console.WriteLine("room number  :" + roomNumber);
+            Console.WriteLine("room type" + roomType);
+            Console.WriteLine("price " + pricePerNight);
+            Console.WriteLine("status:" + isAvaiable);
+             
+        }
     }
 }
